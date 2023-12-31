14de2430a19ac5ee151d37b53b743c1c7b8fbebc
Amar Hussain <99237742+amarhussain01@users.noreply.github.com>
2023-12-31 00:04:48 +0000

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+let title;
+let message;
+title = "Molly's Special Offers";
+message = '<a href=\"sale.html\">25% off!</a>';
+
+let elTitle = document.getElementById('title');
+elTitle.innerHTML = title;
+let elNote = document.getElementById('note');
+elNote.innerHTML = message;
